0064214e3664684ec9e0128a0e02816b8f5cf14c
Arthur Henrique Mazzotti <arthmzz@gmail.com>
2025-05-26 10:35:01 -0300

    Ajusta app.py para rodar no Render com porta dinâmica e host 0.0.0.0

--- app.py
+++ app.py
@@ -134,4 +134,5 @@ def stream_vods(stream_key):
     ffmpeg_process = None
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=False)
